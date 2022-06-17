89d173b624dc931f70c793f47a58932d1400212c
ammar ramadhan <ammar.ramadhan@zeniuseducation.com>
2022-06-17 14:00:24 +0700

    add counter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var counter = 0;
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Hellooo' });
+  res.render('index', {
+    title: 'Hellooo', counter: (function () {
+      counter = counter + 1;
+      return counter;
+    })()
+  });
 });
+
+
 
 module.exports = router;
