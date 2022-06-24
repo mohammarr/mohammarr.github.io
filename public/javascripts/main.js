79cf287fd5627e23258f20ffcff5296931b072d2
ammar ramadhan <ammar.ramadhan@zeniuseducation.com>
2022-06-24 11:13:28 +0700

    update canvas

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,9 @@
 const canvas = document.getElementById("myCanvas");
-// canvas.width = 200;
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 
 const ctx = canvas.getContext("2d");
+
 let balls = [
     new Ball(window.innerWidth / 2, window.innerHeight / 3)
 ];
