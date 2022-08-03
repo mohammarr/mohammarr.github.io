3b994cdf187fd7a3827e8a3b478770bb6c15c418
Mohammad Ammar Ramadhan <ammar.ramadhan@zeniuseducation.com>
2022-08-03 12:18:47 +0700

    test date timezone

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,15 @@ var router = express.Router();
 
 var counter = 0;
 
+function getStartEndOfDay() {
+  let yesterday = new Date();
+  yesterday = yesterday.setDate(yesterday.getDate() - 1);
+  let s = new Date(new Date(yesterday).setUTCHours(-7, 0, 0, 0));
+  let e = new Date(new Date(yesterday).setUTCHours(16, 59, 59, 99));
+  return [s.toISOString(), e.toISOString()]
+}
+
+var [startTime, endTime] = getStartEndOfDay()
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
@@ -10,6 +19,16 @@ router.get('/', function (req, res, next) {
       counter = counter + 1;
       return counter;
     })()
+  });
+});
+
+router.get('/date', function (req, res, next) {
+  res.render('date', {
+    title: 'Mohammad Ammar Ramadhan',
+    now: new Date(),
+    yesterday: new Date(new Date().setDate(new Date().getDate() - 1)),
+    startTime: startTime,
+    endTime: endTime
   });
 });
 
